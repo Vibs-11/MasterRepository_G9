a16d20f7c27b3deb54278288d5c239333ddc9943
MuskanPunia <157521521+MuskanPunia@users.noreply.github.com>
2024-03-06 10:46:08 +0530

    Muskan_2310990743.py
    Accept my pull request

--- PythonFunctionCalculator.py
+++ PythonFunctionCalculator.py
@@ -1,28 +1,44 @@
-print("Calculator to perform operation:: ")
-print(" Select a number to perform corresponding action: ")
-print(
-  " 1 for multiplication, 2 for division, 3 for subtraction, 4 for exponent:: "
-)
-
-
-def multiplication(num1, num2):
-  mul = num1 * num2
-  print("Multiplication of the two nos. is ::", mul)
-
-
-Operation = int(input("Select the number from 1, 2, 3, 4 ::"))
-
-num1 = int(input("Enter the first number::"))
-num2 = int(input("Enter the Second number::"))
-
-if (Operation == 1):
-  multiplication(num1, num2)
-
-elif (Operation == 2):
-  division(num1, num2)
-
-elif (Operation == 3):
-  subtraction(num1, num2)
+import math
+a= float(input("Enter the value of a "))
+b=float(input("Enter the value of b "))
+ope=input("Select the operator ")
+if ope =='+':
+    print("Your answer",a+b)
+elif ope =='-':
+    print("Your answer",a-b)
+elif ope =='*':
+    print("Your answer",a*b)
+elif ope=='/':
+    print("Your answer",a/b)
+elif ope == "^":
+    output = math.pow(a,b)
+    print("Your Answer: "+str(output))
+elif ope == "/-":
+    output = math.sqrt(a)
+    print("Your Answer: "+str(output))
+elif ope == "sin":
+    if(1>=a>=0):
+        output = math.sin(a)
+        print("Your Answer: "+str(output))
+    elif(a>1):
+        print("Out of Range")
+elif ope == "cos":
+    if(1>=a>=0):
+        output = math.cos(a)
+        print("Your Answer: "+str(output))
+    elif(a>1):
+        print("Out of Range")
+elif ope == "tan":
+    output = math.tan(a)
+    print("Your Answer: "+str(output))
+elif ope == "log10":
+    output = math.log10(a)
+    print("Your Answer: "+str(output))
+elif ope == "log":
+    output = math.log(b,a)
+    print("Your Answer: "+str(output))
 
 else:
-  exponent(num1, num2)
+    print("Invalid")
+
+
