5c7d39c79aa1f31647e2b6ab5cf94e9abb09e6e1
Liza1334 <156873212+Liza1334@users.noreply.github.com>
2024-03-06 10:40:42 +0530

    LizaGarg_2310991334.py

--- Liza_2310991334.py
+++ Liza_2310991334.py
@@ -45,7 +45,7 @@ def calculator():
         print("\nOperations:")
         print("1. Addition")
         print("2. Subtraction")
-        print("3. Multiplication")
+        print("3. Multiplication.")
         print("4. Division")
         print("5. Square Root")
         print("6. Power")
