22db8426b03914a88dd62219aad2bf8accf2bf31
Liza1334 <156873212+Liza1334@users.noreply.github.com>
2024-03-06 10:42:55 +0530

    LizaGarg_2310991334.py
    Accept my pull request

--- Liza_2310991334.py
+++ Liza_2310991334.py
@@ -13,7 +13,7 @@ def divide(x, y):
     if y != 0:
         return x / y
     else:
-        return "Cannot divide by zero"
+        return "Cannot divide by zero."
 
 def square_root(x):
     return math.sqrt(x)
