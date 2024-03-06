be2a3867c897150b56a95299d071f28cbcb558fe
BhardwajJatin2109 <jatin2109.be23@chitkara.edu.in>
2024-03-06 14:48:43 +0530

    Added Factorial Feature

--- Jatin_Bhardwaj_2310992109.py
+++ Jatin_Bhardwaj_2310992109.py
@@ -2,7 +2,7 @@ print("**********************************************************************")
 print("                :::::::::::::::Calculator:::::::::::::::\n")
 print("             ::::Select a number to perform corresponding action:::: ")
 print(
-  " [1] for multiplication \n [2] for division \n [3] for subtraction \n [4] for exponent \n [5] for addition\n "
+  " [1] for multiplication \n [2] for division \n [3] for subtraction \n [4] for exponent \n [5] for addition\n [6] for factorial\n "
 )
 
 
@@ -21,15 +21,34 @@ def exponent(num1, num2):
 def addition(num1, num2):
     add = num1 + num2
     print("Addition of the two nos. is ::", add)
+def factorial(num1):
+    fact = 1
+    for i in range(1,num1+1):
+        fact=fact*i
+    print("Factorial of First Number is=", end=" ")
+    print(fact)
+def factorial2(num2):
+    fact1 = 1
+    for j in range(1,num2+1):
+        fact1=fact1*j
+    print("Factorial of First Number is=", end=" ")
+    print(fact1)
+    
+#     fact1 = 1
+#     for j in range(1,num2+1):
+#         fact2=fact*j
+#     print("Factorial of Second Number is=", end=" ")
+#     print(fact2)
+    
 
 
-Operation = int(input("Select the number from 1, 2, 3, 4, 5 :: "))
+Operation = int(input("Select the number from 1, 2, 3, 4, 5, 6 :: "))
 
 num1 = int(input("Enter the first number:: "))
 num2 = int(input("Enter the Second number:: "))
 
 if (Operation == 1):
-  multiplication(num1, num2)
+ multiplication(num1, num2)
 
 elif (Operation == 2):
   division(num1, num2)
@@ -37,5 +56,13 @@ elif (Operation == 2):
 elif (Operation == 3):
   subtraction(num1, num2)
 
-else:
+elif (Operation == 4):
   exponent(num1, num2)
+
+elif (Operation == 5):
+    addition(num1, num2)
+
+else:
+  factorial(num1)
+  factorial2(num2)
+
