7bce90269b09b4478e5945d825331695ec45e3e8
Nitish Mehan <nitish0589.be23@chitkara.edu.in>
2024-03-14 15:47:15 +0530

    s

--- 2310990589_Nitish_Mehan.py
+++ 2310990589_Nitish_Mehan.py
@@ -1,96 +1,150 @@
 from tkinter import *
-expression = "" 
-def press(num): 
-	global expression 
-	expression = expression + str(num) 
-	equation.set(expression) 
-def equalpress(): 
-	try: 
-		global expression 
-		total = str(eval(expression)) 
-		equation.set(total) 
-		expression = "" 
-	except: 
-		equation.set(" error ") 
-		expression = "" 
-def clear(): 
-	global expression 
-	expression = "" 
-	equation.set("") 
-if __name__ == "__main__": 
-	gui = Tk() 
-	gui.configure(background="light green") 
-	gui.title("Simple Calculator") 
-	gui.geometry("270x150") 
-	equation = StringVar() 
-	expression_field = Entry(gui, textvariable=equation) 
-	expression_field.grid(columnspan=4, ipadx=70)
-	button1 = Button(gui, text=' 1 ', fg='black', bg='blue', 
-					command=lambda: press(1), height=1, width=7) 
-	button1.grid(row=2, column=0) 
 
-	button2 = Button(gui, text=' 2 ', fg='black', bg='blue', 
-					command=lambda: press(2), height=1, width=7) 
-	button2.grid(row=2, column=1) 
+root = Tk()
+root.geometry("300x400")
+root.title("SHUBHULATOR")
 
-	button3 = Button(gui, text=' 3 ', fg='black', bg='blue', 
-					command=lambda: press(3), height=1, width=7) 
-	button3.grid(row=2, column=2) 
+bar = Entry(root,width=22,font=("Ubuntu",18,"normal"),fg="blue",bg="Pale Turquoise")
+bar.place(x=5,y=3)
 
-	button4 = Button(gui, text=' 4 ', fg='black', bg='blue', 
-					command=lambda: press(4), height=1, width=7) 
-	button4.grid(row=3, column=0) 
+X = 75
+Y = 75
 
-	button5 = Button(gui, text=' 5 ', fg='black', bg='blue', 
-					command=lambda: press(5), height=1, width=7) 
-	button5.grid(row=3, column=1) 
+###################################################################################################
 
-	button6 = Button(gui, text=' 6 ', fg='black', bg='blue', 
-					command=lambda: press(6), height=1, width=7) 
-	button6.grid(row=3, column=2) 
+def insert(num):
+    bar['fg']="blue"
+    text = bar.get()
+    if (text.split() == []) and (num=="/" or num=="*" or num==")"):
+        num = ''
+        
+    elif (text.endswith('*')) and (num=="+" or num=="-" or num=="/" or num=="*"):
+        BackSpace()
+    elif (text.endswith('/')) and (num=="+" or num=="-" or num=="/" or num=="*"):
+        BackSpace()
+    elif (text.endswith('+')) and (num=="+" or num=="-" or num=="/" or num=="*"):
+        BackSpace()
+    elif (text.endswith('-')) and (num=="+" or num=="-" or num=="/" or num=="*"):
+        BackSpace()
 
-	button7 = Button(gui, text=' 7 ', fg='black', bg='blue', 
-					command=lambda: press(7), height=1, width=7) 
-	button7.grid(row=4, column=0) 
+    elif (text.endswith('.')) and (num=="." or num=="+" or num=="-" or num=="/" or num=="*"):
+        num=''
+    bar.insert(END,num)
 
-	button8 = Button(gui, text=' 8 ', fg='black', bg='blue', 
-					command=lambda: press(8), height=1, width=7) 
-	button8.grid(row=4, column=1) 
+def BackSpace():
+    bar['fg']="blue"
+    try:
+        text = bar.get()
+        l = list(text)
+        l.pop()
+        Text = ""
+        for i in range(len(l)):
+            Text += l[i]
+        bar.delete(0,END)
+        bar.insert(0,Text)
+    except:
+        None
+        
+def Delete():
+    bar['fg']="blue"
+    bar.delete(0,END)
 
-	button9 = Button(gui, text=' 9 ', fg='black', bg='blue', 
-					command=lambda: press(9), height=1, width=7) 
-	button9.grid(row=4, column=2) 
+def BracketCheck():
+    text = str(bar.get())
+    Text = list(text)
+    a=0
+    for i in range(len(Text)):
+        if Text[i] == "(":
+            a+=1
+    b=0
+    for i in range(len(Text)):
+        if Text[i] == ")":
+            b+=1
+    Add = a-b
+    return Add
 
-	button0 = Button(gui, text=' 0 ', fg='black', bg='blue', 
-					command=lambda: press(0), height=1, width=7) 
-	button0.grid(row=5, column=0) 
+def Answer():
+    text = str(bar.get())
 
-	plus = Button(gui, text=' + ', fg='black', bg='blue', 
-				command=lambda: press("+"), height=1, width=7) 
-	plus.grid(row=2, column=3) 
+    Add = BracketCheck()
+    if Add>0:
+        bar.insert(END,Add*")")
+    
+    else:
+        try:
+            answer = eval(text)
+            Delete()
+            bar.insert(0,answer)
+            bar['fg'] = "forestgreen"
+        except:
+            bar['fg'] = "red"
 
-	minus = Button(gui, text=' - ', fg='black', bg='blue', 
-				command=lambda: press("-"), height=1, width=7) 
-	minus.grid(row=3, column=3) 
+###################################################################################################
 
-	multiply = Button(gui, text=' * ', fg='black', bg='blue', 
-					command=lambda: press("*"), height=1, width=7) 
-	multiply.grid(row=4, column=3) 
+n1 = Button(root,text="1",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("1"))
+n1.place(x=0,y=Y+20)
 
-	divide = Button(gui, text=' / ', fg='black', bg='blue', 
-					command=lambda: press("/"), height=1, width=7) 
-	divide.grid(row=5, column=3) 
+n2 = Button(root,text="2",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("2"))
+n2.place(x=X,y=Y+20)
 
-	equal = Button(gui, text=' = ', fg='black', bg='blue', 
-				command=equalpress, height=1, width=7) 
-	equal.grid(row=5, column=2) 
+n3 = Button(root,text="3",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("3"))
+n3.place(x=2*X,y=Y+20)
 
-	clear = Button(gui, text='Clear', fg='black', bg='blue', 
-				command=clear, height=1, width=7) 
-	clear.grid(row=5, column='1') 
+n4 = Button(root,text="4",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("4"))
+n4.place(x=0,y=(2*Y)+20)
 
-	Decimal= Button(gui, text='.', fg='black', bg='blue', 
-					command=lambda: press('.'), height=1, width=7) 
-	Decimal.grid(row=6, column=0) 
-	# start the GUI 
-	gui.mainloop() 
+n5 = Button(root,text="5",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("5"))
+n5.place(x=X,y=(2*Y)+20)
+
+n6 = Button(root,text="6",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("6"))
+n6.place(x=2*X,y=(2*Y)+20)
+
+n7 = Button(root,text="7",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("7"))
+n7.place(x=0,y=(3*Y)+20)
+
+n8 = Button(root,text="8",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("8"))
+n8.place(x=X,y=(3*Y)+20)
+
+n9 = Button(root,text="9",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("9"))
+n9.place(x=2*X,y=(3*Y)+20)
+
+n0 = Button(root,text="0",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light cyan",command=lambda:insert("0"))
+n0.place(x=X,y=(4*Y)+20)
+
+####################################################################################################
+
+dot = Button(root,text=".",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="gray85",command=lambda:insert("."))
+dot.place(x=0,y=(4*Y)+20)
+
+equal = Button(root,text="=",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="green",command=Answer)
+equal.place(x=2*X,y=(4*Y)+20)
+
+####################################################################################################
+
+mult = Button(root,text="X",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light green",command=lambda:insert("*"))
+mult.place(x=3*X,y=Y+20)
+
+div = Button(root,text="/",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light green",command=lambda:insert("/"))
+div.place(x=3*X,y=(2*Y)+20)
+
+plus = Button(root,text="+",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light green",command=lambda:insert("+"))
+plus.place(x=3*X,y=(3*Y)+20)
+
+minus = Button(root,text="-",font=("Courier New",16,"bold"),padx = 20,pady = 16,bd = 4,bg="light green",command=lambda:insert("-"))
+minus.place(x=3*X,y=(4*Y)+20)
+
+#####################################################################################################
+
+AC = Button(root,text="AC",font=("Courier New",16,"bold"),padx = 14,pady = 6,bd = 4,bg="light yellow",command=Delete)
+AC.place(x=0,y=Y-35)
+
+back = Button(root,text=u"\u2190",font=("Courier New",16,"bold"),padx = 20,pady = 6,bd = 4,bg="light yellow",command=BackSpace)
+back.place(x=X,y=Y-35)
+
+Open = Button(root,text="(",font=("Courier New",16,"bold"),padx = 20,pady = 6,bd = 4,bg="light yellow",command=lambda:insert("("))
+Open.place(x=2*X,y=Y-35)
+
+Close = Button(root,text=")",font=("Courier New",16,"bold"),padx = 20,pady = 6,bd = 4,bg="light yellow",command=lambda:insert(")"))
+Close.place(x=3*X,y=Y-35)
+
+root.mainloop()
